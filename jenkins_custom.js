f6383200a9bac2a0befa4603390a5a67b23bc3fb
anuragpurohit86 <anurag.purohit@atos.net>
2015-06-03 14:53:34 +0200

    Update jenkins_custom.js

--- jenkins_custom.js
+++ jenkins_custom.js
@@ -1,3 +1,7 @@
 jQuery(document).ready(function() {
     console.log( "ready!" );
+    jQuery("#jenkins-home-link").append( "<div id="headerTop">
+				<div class="logo"><span class="AtosSpan"></span></div>
+				<div class="devops"><span class="AtosSpan"></span></div>  
+			</div>" );
 });
